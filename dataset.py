e312cd4b7284d0189750ff88137f29a2f89563b7
Steven John <stevenjohn@Stevens-MacBook-Pro.local>
2020-01-26 17:49:08 +0530

    Made dataset.py main file

--- dataset.py
+++ dataset.py
@@ -14,6 +14,35 @@ JSON_FILES = ['summer trees.json', 'winter trees.json']
 FOLDER_NAMES = ['dataset/summer trees/', 'dataset/winter trees/']
 
 
+async def google_search_and_download(q, session, offset=0, folder_name=None):
+
+    if folder_name is None:
+        folder_name = q
+
+    os.makedirs(f"dataset/{folder_name}", exist_ok=True)
+
+    headers = {"apikey": os.getenv("ZENSERP_KEY")}
+
+    params = (
+        ("q", folder_name),
+        ("device", "desktop"),
+        ("tbm", "isch"),
+        ("tbs", "itp:photo,isz:l")
+    )
+    async with session.get(
+        url="https://app.zenserp.com/api/v2/search",
+        headers=headers,
+        params=params
+    ) as response:
+
+        results = await response.json()
+
+    json_file_name = f"{q}.json"
+    json.dump(results, open(json_file_name, 'w'), indent=2)
+
+    JSON_FILES.append(json_file_name)
+
+
 def create_dataset_folders(folder_names):
     for folder_name in folder_names:
         os.makedirs(folder_name, exist_ok=True)
@@ -57,7 +86,7 @@ async def download_images_from_json(json_files, folder_names):
 
                 for index in range(len(json_files))
                 for image_obj in
-                json.load(open(json_files[index], 'r'))['image_results']
+                json.load(open(json_files[index], 'r'))['image_results'][:5]
             ]
 
         await asyncio.gather(*tasks)
