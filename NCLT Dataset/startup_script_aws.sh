4c232db35577018cc1f714703590124e4947e8a4
Steven John <stevenjohn@Stevens-MacBook-Pro.local>
2020-02-20 07:56:32 +0530

    Updated startup script

--- NCLT Dataset/startup_script_aws.sh
+++ NCLT Dataset/startup_script_aws.sh
@@ -1,1 +1,3 @@
 curl -o prep.py https://raw.githubusercontent.com/StevenJPx2/s8-project/master/NCLT%20Dataset/prep_nclt_dataset.py;
+screen -dmS preppy
+screen -S preppy -p 0 -X stuff $'python3 prep.py\n'
